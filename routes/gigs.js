10869f47e35386f40d4e706c06e5ecddf6ad4f0a
Stephen Cavender <6619472+StephenCavender@users.noreply.github.com>
2021-06-09 10:02:15 -0400

    create add route with dummy data

--- routes/gigs.js
+++ routes/gigs.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const db = require('../config/database');
 const Gig = require('../models/Gig');
 
+// Get gig list
 router.get('/', (req, res) =>
   Gig.findAll()
     .then((gigs) => {
@@ -12,5 +13,30 @@ router.get('/', (req, res) =>
     })
     .catch((err) => console.log(err))
 );
+
+// Add a gig
+router.get('/add', (req, res) => {
+  const data = {
+    title: 'iOS app',
+    technologies: 'swift,java,mobile',
+    budget: '2300',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    contact_email: 'steve2@email.com',
+  };
+
+  let { title, technologies, budget, description, contact_email } = data;
+
+  // insert into table
+  Gig.create({
+    title,
+    technologies,
+    budget,
+    description,
+    contact_email,
+  })
+    .then((gig) => res.redirect('/gigs'))
+    .catch((err) => console.log(err));
+});
 
 module.exports = router;
